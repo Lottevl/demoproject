04f698d7b1f087b549aa1bf2b020c83a27689f6c
Lotte Van Lint <lottevanlint@MacBook-Pro-van-Lotte.local>
2019-03-13 15:46:20 +0100

    first commit

--- _FRONT_/src/app/app.component.ts
+++ _FRONT_/src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
-
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+selector: 'app-root',
+templateUrl: './app.component.html',
+styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'FrontEnd';
+	studenten = ["Joey", "Vlad", "Marlene", "Lotte"];
+	
+	vakken = ["HTML", "CSS", "PHP", "Bootstrap"];
+	
+	fouten = ["kleine fouten", "medium fouten", "grote fouten"];
+	
+	clickbtn(){
+		console.log("fout toevoegen");
+	}
 }
